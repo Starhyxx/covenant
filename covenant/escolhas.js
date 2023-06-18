26b3b6c7adfdd7c12c42ed86e0d9a7ad31578206
Sidrim <136641647+Starhyxx@users.noreply.github.com>
2023-06-18 18:19:41 -0400

    escolhas.js

--- covenant/escolhas.js
+++ covenant/escolhas.js
@@ -1,4 +1,5 @@
 
+
   document.addEventListener('DOMContentLoaded', function() {
     const menuButton = document.querySelector('.botao_menu');
     const telaDesfoque = document.querySelector('.tela_desfoque');
@@ -22,5 +23,23 @@
     });
   });
 
-
+  document.addEventListener('DOMContentLoaded', function() {
+    const personagensString = localStorage.getItem('personagem');
+    const personagens = personagensString ? JSON.parse(personagensString) : [];
+  
+    const ultimoPersonagem = personagens.length > 0 ? personagens[personagens.length - 1] : {};
+  
+    const { vitalidade, sanidade } = ultimoPersonagem;
+  
+    const valorVitalidadeSpan = document.getElementById('valorVitalidade');
+    const valorSanidadeSpan = document.getElementById('valorSanidade');
+  
+    if (valorVitalidadeSpan) {
+      valorVitalidadeSpan.textContent = vitalidade ? vitalidade.toString() + '%' : '';
+    }
+  
+    if (valorSanidadeSpan) {
+      valorSanidadeSpan.textContent = sanidade ? sanidade.toString() + '%' : '';
+    }
+  });
 
